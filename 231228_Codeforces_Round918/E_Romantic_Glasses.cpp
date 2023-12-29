52903fee3274199bcad456b356128352e279cecd
kimkun07 <kimkun07@naver.com>
2023-12-29 10:14:59 +0900

    Solve: E

--- 231228_Codeforces_Round918/E_Romantic_Glasses.cpp
+++ 231228_Codeforces_Round918/E_Romantic_Glasses.cpp
@@ -1,37 +1,42 @@
 #include <cstdio>
 #include <iostream>
 #include <vector>
+#include <set>
 using namespace std;
 typedef long long ll;
 
 int a[200'005];
-ll odd[200'005], even[200'005];
 
 void solve(int test){
     int n; cin >> n;
     for(int i = 1; i <= n; ++i){
         cin >> a[i];
     }
-
-    even[0] = odd[0] = 0;
-    for(int i = 1; i <= n; ++i){
-        even[i] = even[i - 1];
-        odd[i] = odd[i - 1];
-        if (i % 2 == 0) {
-            even[i] += a[i];
-        } else {
-            odd[i] += a[i];
-        }
-    }
     
-    // (j+1)..i
+    set<ll> diff;
+    // (j+1)..i = sum[i] - sum[j]
+    // condition: even in (j+1)..i == odd in (j+1)..i
+    // -> even[i] - even[j] == odd[i] - odd[j]
+    // -> even[j] - odd[j] == even[i] - odd[i]
+    // there exists (even[j] - odd[j]) which matches (even[i] - odd[i])
+
+    ll evenAcc = 0, oddAcc = 0;
+    diff.insert(0); // sum[0]
+    
     for(int i = 1; i <= n; ++i){
-        for(int j = 0; j < i; ++j){
-            if (even[i] - even[j] == odd[i] - odd[j]) {
-                cout << "YES" << endl;
-                return;
-            }
+        if (i % 2 == 0) {
+            evenAcc += a[i];
+        } else {
+            oddAcc += a[i];
         }
+        
+        const ll d = evenAcc - oddAcc;
+        const bool is_in = diff.contains(d);
+        if (is_in) {
+            cout << "YES" << endl;
+            return;
+        }
+        diff.insert(d);
     }
     cout << "NO" << endl;
 }
