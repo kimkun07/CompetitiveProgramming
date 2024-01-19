4aa53fac30d4931d0012c650265d9317746867a0
kimkun07 <kimkun07@naver.com>
2024-01-19 17:24:08 +0900

    C: Submission 242399685

--- 240102_Codeforces_Round917/C_Watering_an_Array.cpp
+++ 240102_Codeforces_Round917/C_Watering_an_Array.cpp
@@ -1,22 +1,16 @@
-#include <iostream>
-#include <map>
-#include <set>
-#include <vector>
-
-using namespace std;
-typedef long long ll;
+#include <bits/stdc++.h>
 
 int a[2005];
 int v[10'005];
 
 void solve(int testcase) {
   int n, k, d;
-  cin >> n >> k >> d;
+  std::cin >> n >> k >> d;
   for (int i = 1; i <= n; ++i) {
-    cin >> a[i];
+    std::cin >> a[i];
   }
   for (int i = 1; i <= k; ++i) {
-    cin >> v[i];
+    std::cin >> v[i];
   }
 
   // n: 2000
@@ -28,7 +22,8 @@ void solve(int testcase) {
   // day=2n까지만 flush를 참았을 때를 확인하면 된다.
   // flush를 참은 경우 최대 n까지 획득 가능한데,
   // 한 번도 안 참은 경우에도 2n일 동안 n만큼 획득할 수 있기 때문이다.
-  for (int i = 1; i <= min(n + 1, d); ++i) {
+  int finalDay = (2 * n < d) ? (2 * n) : d; // min(2 * n, d)
+  for (int i = 1; i <= finalDay; ++i) {
     // day=i 일 때 처음 flush한 경우, 총점 scoreWhenFlush 획득
     int cnt = 0;
     for (int j = 1; j <= n; ++j) {
@@ -39,7 +34,7 @@ void solve(int testcase) {
     // 한 번 flush한 이후에는 어차피 한 번에 1점 이상 획득할 수 없다.
     // acc -> flush -> acc -> flush 로 1점씩 획득하기
     int scoreWhenFlush = cnt + (d - i) / 2;
-    result = max(scoreWhenFlush, result);
+    result = (scoreWhenFlush > result) ? scoreWhenFlush : result;
     
     
     // flush 하지 않은 경우, 1..b acc
@@ -49,15 +44,15 @@ void solve(int testcase) {
     }
   }
 
-  cout << result << "\n";
+  std::cout << result << std::endl;
 }
 
 int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(0);
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(0);
 
   int T;
-  cin >> T;
+  std::cin >> T;
   for (int t = 1; t <= T; ++t) {
     solve(t);
   }
