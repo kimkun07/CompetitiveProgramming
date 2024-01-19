11fc283b79e3ff2a8a00881ec01b99626ca134ee
kimkun07 <kimkun07@naver.com>
2024-01-19 13:54:40 +0900

    C: Format

--- 240102_Codeforces_Round917/C_Watering_an_Array.cpp
+++ 240102_Codeforces_Round917/C_Watering_an_Array.cpp
@@ -10,19 +10,24 @@ int a[2005];
 int v[10'005];
 
 void solve(int testcase) {
-  int n, k, d; cin >> n >> k >> d;
-  for(int i = 1; i <= n; ++i){
+  int n, k, d;
+  cin >> n >> k >> d;
+  for (int i = 1; i <= n; ++i) {
     cin >> a[i];
   }
-  for(int i = 1; i <= k; ++i){
+  for (int i = 1; i <= k; ++i) {
     cin >> v[i];
   }
-  
+
+  // n: 2000
+  // k: 10^5
+  // d: 10^9
+
   // acc action, flush action
   int result = 0;
   // a[i] gets score when cnt(b[j] >= i) == i
   // a[i] <= n -> b[i]도 n번만 확인하면 된다.
-  for(int i = 1; i <= min(n + 1, d); ++i){
+  for (int i = 1; i <= min(n + 1, d); ++i) {
     // 1) i-th day: very first flush
     // 최초의 경우: 잘하면 고득점할 수 있다.
     int cnt = 0;
@@ -33,17 +38,17 @@ void solve(int testcase) {
     }
 
     // 나머지 경우: 어차피 한 번에 1점 이상 획득할 수 없다.
-    // add -> count -> add -> count 로 1점씩 획득하기
+    // acc -> flush -> acc -> flush 로 1점씩 획득하기
     int t = cnt + (d - i) / 2;
     result = max(t, result);
-  
+
     // 2) Assume acc action on i-th day
     const int b = v[(i - 1) % k + 1];
-    for (int j = 1; j <= b; ++j) {
+    for (int j = 1; j <= b; ++j) { // b <= n
       a[j]++;
     }
   }
-  
+
   cout << result << "\n";
 }
 
