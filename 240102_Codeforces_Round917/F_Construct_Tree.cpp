a21e5bf4c2967d9b078d93f84a61dfe6194f6b8f
kimkun07 <kimkun07@naver.com>
2024-01-23 17:28:39 +0900

    F: TLE on 13 (Use two state knapsack from solution)

--- 240102_Codeforces_Round917/F_Construct_Tree.cpp
+++ 240102_Codeforces_Round917/F_Construct_Tree.cpp
@@ -1,4 +1,4 @@
-#include <array>
+#include <functional>
 #include <iostream>
 #include <vector>
 
@@ -15,25 +15,6 @@ void printAns(bool yes) {
   }
 }
 
-// dp: [1.N][1..W], v: [1..N], w: [1..N]
-void knapsack(int N, int W, vector<vector<int>> &dp, const vector<int> &v,
-              const vector<int> &w) {
-  for (int i = 1; i <= N; ++i) {
-    int value = v[i];
-    int weight = w[i];
-    for (int j = 1; j <= W; ++j) {
-      if (j - weight == 0 or (j - weight >= 0 and dp[i - 1][j - weight] > 0)) {
-        // previous should be valid choice
-        dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - weight] + value);
-      } else {
-        // previous was not valid choice
-        // Cannot take
-        dp[i][j] = dp[i - 1][j];
-      }
-    }
-  }
-}
-
 void solve(int testcase) {
   int n, d;
   cin >> n >> d;
@@ -43,74 +24,89 @@ void solve(int testcase) {
   }
 
   // Solve
-  // 1. knapsack -> find diameter of d
-  // 2. we can put leftover nodes in half position of diameter.
-  // 3. Check 2 doesn't increase diameter of the tree -> second knapsack
-  // [Second Knapsack]
-  // split diameter into half (knapsack)
-  // consider two length as a and b
-  // diameter doesn't increase iff a >= leftover, b >= leftover
-  // we have to check split diameter so that a >= max(leftover) b >= leftover
-  // [First Knapsack]
-  // If there are several valid knapsack cases,
-  // it is okay to greedily take largest l possible
-  // Let's suppose taking largest element `ml` leads to wrong answer.
-  // This means second larges element `ml2` doesn't match condition 3.
-  // Split a = ml, b = d - ml, then a >= ml2 but b < ml2
-  // b = d - ml < ml2, ml + ml2 > d
-  // =>  ml > d / 2  (Because ml >= ml2)
-  // If we had not take ml, ml cannot match condition 3 after all.
+  // 1. knapsack -> find diameter of d using l[1..n]
+  // 2. put leftover nodes between diameter
+  //    we should check this doesn't increase diameter
+  //
+  // 2-A. if mx is included, leftover will not increase diameter
+  //      -> simple knapsack
+  // 2-B. if mx is not included, we should check every possible (left, right)
+  //      left + right will be diameter
+  //      new item[i] can either be put in left or right
+  //      -> knapsack with two states (left, right)
 
-  // 0. Sort descending, so that big number is taken if possible in knapsack
-  auto lama = [](const int &front, const int &back) -> bool {
-    return front > back;
-  };
-  // works well excluding l[0]
-  sort(l.begin() + 1, l.end(), lama);
-
-  // 1. knapsack
-  vector<vector<int>> dp(n + 1, vector<int>(d + 1));
-  knapsack(n, d, dp, l, l);
-
-  if (dp[n][d] == 0) {
+  // Works well excluding l[0]
+  sort(l.begin() + 1, l.end());
+  if (l[n] + l[n - 1] > d) {
+    // always bigger than diameter
     printAns(false);
     return;
   }
 
-  // 2. backtrack for find diameter(taken) or leftover(not taken)
-  vector<int> diameter;
-  int notTakenMax = 0;
-  int weight = d;
-  for (int i = n; i >= 1; i--) {
-    if (dp[i][weight] == dp[i - 1][weight]) {
-      // works well with weight == 0
-      notTakenMax = max(l[i], notTakenMax);
-    } else {
-      // taken
-      diameter.push_back(l[i]);
-      weight -= l[i];
+  int mx = l.back();
+  {
+    // Knapsack with mx included
+    vector<vector<bool>> dp(n + 1, vector<bool>(d + 1));
+    for (int i = 0; i <= n; ++i) {
+      dp[i][0] = true;
+    }
+
+    for (int i = 1; i <= n - 1; ++i) {
+      int weight = l[i];
+      for (int j = 1; j <= d - mx; ++j) {
+        dp[i][j] = dp[i - 1][j];
+        if (j - weight >= 0) {
+          dp[i][j] = dp[i - 1][j - weight] or dp[i][j];
+        }
+      }
+    }
+
+    if (dp[n - 1][d - mx] and d - mx > 0) {
+      // Construct tree with l[1..n-1] + mx
+      // Exclude if we only used l[n] because we can't handle leftover
+      // Then mx >= leftover and there is always node between diameter
+      printAns(true);
+      return;
     }
   }
 
-  // 3. second knapsack
-  // a, b = splitIntoTwoGroup(diameter)
-  // b = d - a
-  // a, b both should be >= notTakenMax
-  // notTakenMax <= a <= d - notTakenMax
-  int lower = notTakenMax;
-  int upper = d - notTakenMax;
-  // split diameter into lower <= a <= upper -> knapsack
-  int n2 = diameter.size();
-  int d2 = upper;
-  vector<vector<int>> dp2(n2 + 1, vector<int>(d2 + 1));
-  
-  diameter.insert(diameter.begin(), 0); // so that diameter: 1..n2
-  knapsack(n2, d2, dp2, diameter, diameter);
+  vector<vector<bool>> dpA(d + 1, vector<bool>(d + 1));
+  vector<vector<bool>> dpB(d + 1, vector<bool>(d + 1));
 
-  for (int a = lower; a <= upper; ++a) {
-    if (dp2[n2][a] != 0) {
-      printAns(true);
-      return;
+  auto prevDp = ref(dpA), dp = ref(dpB);
+  // Knapsack with two states
+  // for item l[i], we can put item left or right
+  // dp(left, right) = dp(left - l, right) | dp(left, right - l) | dp(left,
+  // right)
+  prevDp.get()[0][0] = dp.get()[0][0] = true;
+  for (int i = 1; i <= n; ++i) {
+    int weight = l[i];
+
+    swap(prevDp, dp);
+    for (int left = 0; left <= d; ++left) {
+      for (int right = 0; right <= d; ++right) {
+        // ex: (30 + 10, 20)
+        dp.get()[left][right] = prevDp.get()[left][right];
+        if (left - weight >= 0) {
+          dp.get()[left][right] =
+              prevDp.get()[left - weight][right] or dp.get()[left][right];
+        }
+        if (right - weight >= 0) {
+          dp.get()[left][right] =
+              prevDp.get()[left][right - weight] or dp.get()[left][right];
+        }
+      }
+    }
+  }
+
+  // Check if mx doesn't increase diameter
+  for (int left = 1; left <= d; ++left) {
+    int right = d - left;
+    if (dp.get()[left][right]) {
+      if (left >= mx and right >= mx) {
+        printAns(true);
+        return;
+      }
     }
   }
 
