1c488137253f9bbd2819b7bb9450adeac19540e3
kimkun07 <kimkun07@naver.com>
2024-01-24 14:32:09 +0900

    C: Solve

--- 240113_Codeforces_Round919/C_Partitioning_the_Array.cpp
+++ 240113_Codeforces_Round919/C_Partitioning_the_Array.cpp
@@ -1,6 +1,4 @@
 #include <iostream>
-#include <map>
-#include <set>
 #include <vector>
 
 using namespace std;
@@ -8,16 +6,68 @@ typedef long long ll;
 
 int a[200'005];
 
-void divisor(int n) {
-  
+int gcd(int a, int b) {
+  // a could be 0
+  if (b == 0) {
+    return a;
+  }
+  return gcd(b, a % b);
+}
+
+// divisor including 1, excluding n
+void get_divisor(int n, vector<int> &result) {
+  for (int i = 1; i < n; ++i) {
+    if (n % i == 0) {
+      result.push_back(i);
+    }
+  }
+  // Todo: use pointer and return
 }
 
 void solve(int testcase) {
-  int n; cin >> n;
-  for(int i = 0; i < n; ++i){
+  int n;
+  cin >> n;
+  for (int i = 0; i < n; ++i) {
     cin >> a[i];
   }
-  cout << "Case " + to_string(testcase) + ": " << "\n";
+
+  // Solve
+  // 1. Get divisor list of n (except n)
+  //    divisor.size() < 20
+  // 2. For each division of subarray, find m = gcd(diff)
+  //    ex) if l = 2
+  //    First element
+  //    a1 - a3 = _ * m + c1
+  //    a3 - a5 = _ * m + c1
+  //    Second element
+  //    a2 - a4 = _ * m + c2
+  //    a4 - a6 = _ * m = c2
+  //    possible iff m is equal to or greater than 2
+
+  vector<int> divisor;
+  get_divisor(n, divisor);
+
+  int ans = 1; // when l == n
+  for (auto &&l : divisor) {
+    // divide a[n] into into subarr[row][col]
+    // subarr has length of l
+    // l != n
+    int subarrEa = n / l;
+    int m = a[0] - a[1 * l + 0];
+    // m could be 0. gcd(a, 0) = gcd(0, a) = a
+    for (int col = 0; col < l; ++col) {
+      for (int row = 1; row < subarrEa; ++row) {
+        int diff = abs(a[col] - a[row * l + col]);
+        m = gcd(m, diff);
+      }
+    }
+    if (m >= 2 or m == 0) {
+      ans += 1;
+    }
+  }
+
+  cout << ans << "\n";
+  // cout << "[TEST OUT] " << ans << endl;
 }
 
 int main() {
