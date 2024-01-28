87ebf3c7d55a01fe4bb67d5ae5492e9c66b08e35
kimkun07 <kimkun07@naver.com>
2024-01-28 17:18:12 +0900

    E: add comments

--- 240113_Codeforces_Round919/E_Counting_Binary_Strings.cpp
+++ 240113_Codeforces_Round919/E_Counting_Binary_Strings.cpp
@@ -21,8 +21,22 @@ void solve(int testcase) {
   int n, k;
   cin >> n >> k;
 
+  // Solve
+  // 1. Convert binary string into array of ints.
+  //    `1` will split zero into arrays.
+  //    ex) 000110000 can be represented as [3, 0, 4]
+  //    Then a[i] + 1 + a[i + 1] <= k (length of substring 00...1..00 <= k)
+  //    Sum of (a[i] + 1) * (a[i+1] + 1) == num (substring number of cases)
+  // 2. Solve as DP[sum][last]
+  //    DP[sum][last]: number of cases for a
+  //    sum = a's current sum, last = last number of a
+  //    (length of a doesn't matter)
+  // 3. [sum-delta][x] -> [sum][last]
+  //    condition: x + 1 + last <= k
+
   vector<vector<ll>> dp(n + 1, vector<ll>(k)); // [0..n][0..k-1]
 
+  // a = [last] is valid
   for (int last = 0; last <= k - 1; ++last) {
     dp[0][last] = 1;
   }
