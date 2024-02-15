db27a3ec277ec15f056fef754f544fca82df6775
kimkun07 <kimkun07@naver.com>
2024-02-15 16:27:43 +0900

    F: Solve

--- 240213_Codeforces_Round925/F.cpp
+++ 240213_Codeforces_Round925/F.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <set>
 #include <vector>
 
 using namespace std;
@@ -14,147 +15,72 @@ void printAns(bool yes) {
   }
 }
 
+bool dfs_cycle(int node, vector<set<int>> &graph, vector<bool> &visited_total,
+               vector<bool> &visited_now) {
+  visited_total[node] = true;
+  visited_now[node] = true;
+
+  for (auto &&v : graph[node]) {
+    if (visited_now[v]) {
+      return true; // there is a cycle
+    } else if (visited_total[v]) {
+      continue; // there is no cycle starting from v
+    } else {
+      bool result = dfs_cycle(v, graph, visited_total, visited_now);
+      if (result) {
+        return true;
+      }
+    }
+  }
+
+  visited_now[node] = false;
+  return false;
+}
+
+bool findCycle(vector<set<int>> &graph) {
+  int n = graph.size() - 1;
+
+  vector<bool> visited_total(n + 1, false); // So we don't have to search again
+  vector<bool> visited_now(n + 1, false);
+
+  for (int i = 1; i <= n; ++i) {
+    if (not visited_total[i]) {
+      bool result = dfs_cycle(i, graph, visited_total, visited_now);
+      if (result) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
 void solve(int testcase) {
   int n, k;
   cin >> n >> k;
 
-  vector<int> correct(n - 1);
-  bool wrong = false;
+  // Solve
+  // For each screenshot, make a graph.
+  // Contradiction of order results in cycle in graph.
+  // If there is no contradiction, graph can be topologically sorted.
+  // (Or there may be one node with no connection)
 
-  int theFirst;
-  {
-    // i = 0
-    cin >> theFirst;
-    for (int j = 0; j < n - 1; ++j) {
-      cin >> correct[j];
-    }
-  }
+  vector<set<int>> graph(n + 1); // 1..n
 
-  if (k == 1) {
-    printAns(not wrong);
-    return;
-  }
-
-  // theFirst is not positioned yet
-  int candidate = -1;
-  {
-    // i = 1
+  for (int i = 0; i < k; ++i) {
     int first;
     cin >> first;
-    vector<int> me(n - 1);
+    vector<int> ai(n - 1); // 0..(n-2)
     for (int j = 0; j < n - 1; ++j) {
-      cin >> me[j];
+      cin >> ai[j];
     }
 
-    int p = 0;
-    for (int j = 0; j < n - 1; ++j) {
-      bool firstFlag = false;
-      if (correct[p] == first) {
-        ++p;
-        firstFlag = true;
-      }
-
-      if (me[j] == correct[p]) {
-        ++p;
-      } else if (me[j] == theFirst) {
-        if (not firstFlag) {
-          // Case 1
-          // p-1    p         p+1
-          // me[j]  theFirst  me[j+1]
-          correct.insert(correct.begin() + p, theFirst);
-          ++p;
-          theFirst = 0;
-        } else {
-          // Case 2
-          // p-2    p-1    p         p+1
-          // me[j]  first  theFirst  me[j+1]
-          // Can be in p or p-1 (No information about first - theFirst)
-          candidate = p - 1; // candidate or candidate + 1
-        }
-      } else {
-        wrong = true;
-      }
+    for (int j = 0; j <= n - 2 - 1; ++j) {
+      // ai[j] -> ai[j + 1] connect
+      graph[ai[j]].insert(ai[j + 1]);
     }
   }
 
-  if (k == 2) {
-    printAns(not wrong);
-    return;
-  }
-
-  {
-    // i = 2
-    int first;
-    cin >> first;
-    vector<int> me(n - 1);
-    for (int j = 0; j < n - 1; ++j) {
-      cin >> me[j];
-    }
-
-    int p = 0;
-    for (int j = 0; j < n - 1; ++j) {
-      bool firstFlag = false;
-      if (correct[p] == first) {
-        ++p;
-        firstFlag = true;
-      }
-
-      if (me[j] == correct[p]) {
-        ++p;
-      } else if (me[j] == theFirst) {
-        if (not firstFlag) {
-          // Case 1
-          // p-1    p         p+1
-          // me[j]  theFirst  me[j+1]
-          if (not(candidate == p or candidate + 1 == p)) {
-            wrong = true;
-          }
-          correct.insert(correct.begin() + p, theFirst);
-          ++p;
-        } else {
-          // Case 2
-          // p-2    p-1    p         p+1
-          // me[j]  first  theFirst  me[j+1]
-          // Can be in p or p-1 (No information about first - theFirst)
-          if (candidate == p) {
-            correct.insert(correct.begin() + p, theFirst);
-            ++p;
-          } else if (candidate == p - 2) {
-            correct.insert(correct.begin() + (p - 1), theFirst);
-            ++p;
-          } else {
-            wrong = true;
-          }
-        }
-      } else {
-        wrong = true;
-      }
-    }
-  }
-
-  for (int i = 3; i < k; ++i) {
-    int first;
-    cin >> first;
-    vector<int> me(n - 1);
-    for (int j = 0; j < n - 1; ++j) {
-      cin >> me[j];
-    }
-
-    int p = 0;
-    for (int j = 0; j < n - 1; ++j) {
-      if (correct[p] == first) {
-        ++p;
-      }
-
-      if (me[j] == correct[p]) {
-        ++p;
-      } else {
-        wrong = true;
-      }
-    }
-  }
-
-  printAns(not wrong);
+  printAns(not findCycle(graph));
 }
 
 int main() {
