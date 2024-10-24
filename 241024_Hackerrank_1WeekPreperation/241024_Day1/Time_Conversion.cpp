3b131aef2ff3a3b6ac50a3fa87c4a2d7271082fb
kimkun07 <kimkun07@naver.com>
2024-10-24 23:25:27 +0900

    Time_Conversion: Solve

--- 241024_Hackerrank_1WeekPreperation/241024_Day1/Time_Conversion.cpp
+++ 241024_Hackerrank_1WeekPreperation/241024_Day1/Time_Conversion.cpp
@@ -9,7 +9,27 @@ using namespace std;
  * The function accepts STRING s as parameter.
  */
 
-string timeConversion(string s) {}
+string timeConversion(string s) {
+  // hh:mm:ssAM or hh:mm:ssPM
+  string hh = s.substr(0, 2);
+  string mm = s.substr(3, 2);
+  string ss = s.substr(6, 2);
+  bool is_pm = (s[8] == 'P');
+
+  int h = stoi(hh);
+  if (h == 12) {
+    h = 0; // 12:00:00AM -> 00:00:00, 12:00:00PM -> 12:00:00
+  }
+  if (is_pm) {
+    h = h + 12;
+  }
+
+  char c[3];
+  sprintf(c, "%02d", h);
+
+  string ans = string(c) + ":" + mm + ":" + ss;
+  return ans;
+}
 
 int main() {
   ofstream fout(getenv("OUTPUT_PATH"));
