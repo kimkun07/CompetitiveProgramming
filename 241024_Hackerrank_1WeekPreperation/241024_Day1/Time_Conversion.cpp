8170cec0fb5eca63aec75351a44e533c45a844f0
kimkun07 <kimkun07@naver.com>
2024-10-24 22:45:36 +0900

    Time_Conversion: skeleton

---
+++ 241024_Hackerrank_1WeekPreperation/241024_Day1/Time_Conversion.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+ * Complete the 'timeConversion' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+string timeConversion(string s) {}
+
+int main() {
+  ofstream fout(getenv("OUTPUT_PATH"));
+
+  string s;
+  getline(cin, s);
+
+  string result = timeConversion(s);
+
+  fout << result << "\n";
+
+  fout.close();
+
+  return 0;
+}
